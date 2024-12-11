9ad9d8170abf7f082d585ef3f56ea8c33609b685
RotarAlexC <rotaralexc@gmail.com>
2024-12-11 20:12:46 +0200

    CRUD Update Contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -47,7 +47,19 @@ const getContact = asyncHandler(async (req, res) => {
 //@access public
 
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update Contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+
+    const updateContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.status(200).json(updateContact);
 });
 
 //@desc Delete contact
